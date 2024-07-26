aaaaf40a448721c953464cab96f2f8491322b1ec
i-prince <molatonny@gmail.com>
2024-07-26 16:19:26 +0300

    Design the html structure of the webpage in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,31 @@ function App() {
   return (
     <>
       <div>
-        
+        <img src='public/avatar-jessica.jpeg'></img>
       </div>
+    
+      <h2>
+        Jessica Randal
+      </h2>
+    
+      <h3>
+        London, United Kingdom
+      </h3>
+
+      <p>
+        "Front-end developer and avid reader"
+      </p>
+
+      <div>
+        <ul>
+          <li>Github</li>
+          <li>Frontend Mentor</li>
+          <li>Linkedin</li>
+          <li>Twitter</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
+
     </>
   )
 }
