1b27b6cd94ff790c03e45c67db2dfecf1a7b85b1
i-prince <molatonny@gmail.com>
2024-07-29 18:53:27 +0300

    Add css Unlink index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,28 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <img src='public/avatar-jessica.jpeg'></img>
+      <div className='profile'>
+        <img src='public/avatar-jessica.jpeg' alt='Jessica Randal' className='profile-image'></img>
       </div>
     
-      <h2>
+      <h2 className='profile-name'>
         Jessica Randal
       </h2>
     
-      <h3>
+      <h3 className='profile-location'>
         London, United Kingdom
       </h3>
 
-      <p>
+      <p className='profile-bio'>
         "Front-end developer and avid reader"
       </p>
 
-      <div>
-        <button>Github</button>
-        <button>Frontend Mentor</button>
-        <button>Linkedin</button>
-        <button>Twitter</button>
-        <button>Instagram</button>
+      <div className='profile-buttons'>
+        <button className='button'>Github</button>
+        <button className='button'>Frontend Mentor</button>
+        <button className='button'>Linkedin</button>
+        <button className='button'>Twitter</button>
+        <button className='button'>Instagram</button>
       </div>
 
     </>
