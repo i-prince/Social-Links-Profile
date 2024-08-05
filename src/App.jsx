db0a94d3bd90ce228973cd3ad61e9d8ea954d798
i-prince <molatonny@gmail.com>
2024-08-05 18:49:53 +0300

    Personalize the project

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,27 @@ function App() {
   return (
     <div className='body'>
       <div className='profile'>
-        <img src='https://rohan843-social-links-profile.netlify.app/assets/images/avatar-jessica.jpeg' alt='Jessica Randal' className='profile-image'></img>
+        <img src='https://lh3.googleusercontent.com/a/ACg8ocJ7k_AIB0TdaDM86rOFHt48wgpHekPuryYqpJEV3BV8_MVtmvMt=s288-c-no' alt='Mola Tonny' className='profile-image'></img>
       </div>
     
       <h2 className='profile-name'>
-        Jessica Randal
+        Mola Tonny
       </h2>
     
       <h3 className='profile-location'>
-        London, United Kingdom
+        Nairobi, Kenya
       </h3>
 
       <p className='profile-bio'>
-        "Front-end developer and avid reader"
+        "Front-end Engineer and ICT Administrator"
       </p>
 
       <div className='profile-buttons'>
-        <button className='button'>Github</button>
-        <button className='button'>Frontend Mentor</button>
-        <button className='button'>Linkedin</button>
-        <button className='button'>Twitter</button>
-        <button className='button'>Instagram</button>
+        <a href='https://github.com/i-prince'><button className='button'>Github</button></a>
+        <a href='https://www.linkedin.com/in/mola-brother-tonny/'><button className='button'>Linkedin</button></a>
+        <a href='https://molatonny.hashnode.dev/'><button className='button'>Hashnode</button></a>
+        <a href='https://dev.to/i-prince'><button className='button'>Dev.to</button></a>
+        <a href='https://x.com/mola_tonny'><button className='button'>Twitter</button></a>
       </div>
 
     </div>
